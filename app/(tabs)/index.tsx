5f8697acefe839dd13efb2e008c108aeea8ddc35
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-14 23:09:46 +0530

    updeta

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,55 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Text, View,StyleSheet,TextInput,Image,Button,Alert } from "react-native";
+import React, { useState } from "react";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+export default function Index() {
+
+  const [state,setState] = useState('');
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View>
+      <Text style={styles.nametext}>Welcome To Store</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Type Here"
+        keyboardType="default"
+        onChange={(e)=>{
+          setState(e.nativeEvent.text)
+        }}
+      />
+       <Image
+        style={styles.tinyLogo}
+        source={require('../../assets/shoenew.jpeg')}
+      />
+      <Button
+        title="Get One"
+        onPress={() => Alert.alert('Add To Card Success')}
+      />
+
+      <Text style={styles.nametext}>{state}</Text>
+
+    </View>
+    
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  nametext:{
+    fontSize:23,
+    color:'red',
+    textAlign:"center"
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input:{
+
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+  tinyLogo: {
+    width: 200,
+    height: 200,
+    marginLeft : 10
+  }
+})
